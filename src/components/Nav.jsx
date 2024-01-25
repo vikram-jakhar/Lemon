b725d73419467a6674cfa4809f0e62f43985b5cb
vikram-jakhar <vikramjakhar144@gmail.com>
2024-01-25 15:45:24 +0530

    add toggle add

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,15 +8,15 @@ const Nav = () => {
     function MobView() {
         setShow(!show)
         if (show === false) {
-            document.body.classNameList.add('overflow-hidden')
+            document.body.classList.add('max-lg:overflow-hidden')
         }
         else {
-            document.body.classNameList.remove('overflow-hidden')
+            document.body.classList.remove('max-lg:overflow-hidden')
         }
     }
     function closeNav() {
         setfirst(false)
-        document.body.classNameList.remove("max-lg:overflow-hidden")
+        document.body.classList.remove("max-lg:overflow-hidden")
     }
     return (
         <>
@@ -28,16 +28,16 @@ const Nav = () => {
                     </div>
                     <div className=' flex items-center gap-[33px] min-[992px]:gap-[70px] min-[1200px]:gap-[170px]'>
                         <ul className={`${show === true ? "left-0" : "left-[-100%]"} flex items-center gap-[33px] mobileview max-[991px]:absolute max-[991px]:top-0 max-[991px]:h-full max-[991px]:justify-center max-[991px]:flex-col max-[991px]:w-full max-[991px]:duration-300 max-[991px]:z-[4] max-[991px]:bg-white`}>
-                            <li><Link to='/' className={` block lg:hidden font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}> Our Services</Link></li>
-                            <li><Link to='/about' className={`  font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>About</Link></li>
-                            <li><Link to='/Blog' className={`  font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>Blog&News</Link></li>
-                            <li><Link to='/Contact ' className={`  font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>Contact </Link></li>
-                            <li><Link to='/Account' className={` block 576:hidden font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>Account</Link></li>
-                            <li className=' flex gap-[11px] 576:hidden'><Phone /><Link to='/tel:+2349067322844' className={`font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>+2349067322844</Link></li>
+                            <li><Link onClick={closeNav} to='/' className={` block lg:hidden font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}> Our Services</Link></li>
+                            <li><Link onClick={closeNav} to='/about' className={`  font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>About</Link></li>
+                            <li><Link onClick={closeNav} to='/Blog' className={`  font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>Blog&News</Link></li>
+                            <li><Link onClick={closeNav} to='/Contact ' className={`  font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>Contact </Link></li>
+                            <li><Link onClick={closeNav} to='/Account' className={` block 576:hidden font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>Account</Link></li>
+                            <li className=' flex gap-[11px] 576:hidden'><Phone /><a href="tel:+2349067322844" className={`font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300 cursor-pointer`}>+2349067322844</a></li>
                         </ul>
                         <ul className=' items-center gap-[30px]  hidden 576:flex'>
                             <li><Link to='/Account' className={` font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>Account</Link></li>
-                            <li className=' flex gap-[11px] border-l border-l-[#000] pl-4'><Phone /><Link to='/tel:+2349067322844' className={`font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300`}>+2349067322844</Link></li>
+                            <li className=' flex gap-[11px] border-l border-l-[#000] pl-4'><Phone /><a href="tel:+2349067322844" className={`font-["poppins"] text-[15px] text-[#000] font-semibold leading-[normal] relative after:absolute after:w-0 after:h-[2px] after:bg-[#B00000] after:left-[50%] after:bottom-[-4px] hover:after:w-[80%] hover:after:left-[8%] after:duration-300 cursor-pointer`}>+2349067322844</a></li>
                         </ul>
                         <label className=" lg:hidden" onClick={MobView}>
                             {show ? (
